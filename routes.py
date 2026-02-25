5128baed1313eddb3b6af0495af69d0fb1bcdca3
Valeria <valeriazhitlukhina@gmail.com>
2026-02-25 14:19:06 +0500

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,14 +1,16 @@
 from aiohttp import web
-from views import get_ad, create_ad, patch_ad, delete_ad
-
-async def root(request):
-    return web.Response(text="Server is running")
-
-routes.get('/')(root)
+from views import register, login, get_ad, create_ad, patch_ad, delete_ad
 
 routes = web.RouteTableDef()
 
+routes.post('/register')(register)
+routes.post('/login')(login)
 routes.get('/advertisements/{id:\d+}/')(get_ad)
 routes.post('/advertisements')(create_ad)
 routes.patch('/advertisements/{id:\d+}/')(patch_ad)
 routes.delete('/advertisements/{id:\d+}/')(delete_ad)
+
+async def root(request):
+    return web.Response(text='Server is running')
+
+routes.get('/')(root)
