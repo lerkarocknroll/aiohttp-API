d118d90c2a386476a4f2ee2847b5410e726e155c
Valeria <valeriazhitlukhina@gmail.com>
2026-02-26 14:31:15 +0500

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,10 +1,11 @@
 from aiohttp import web
-from views import register, login, get_ad, create_ad, patch_ad, delete_ad
+from views import register, login, get_ads_list, get_ad, create_ad, patch_ad, delete_ad  # ИЗМЕНЕНО: добавлен get_ads_list
 
 routes = web.RouteTableDef()
 
 routes.post('/register')(register)
 routes.post('/login')(login)
+routes.get('/advertisements')(get_ads_list)           # ИЗМЕНЕНО: новый маршрут для списка
 routes.get('/advertisements/{id:\d+}/')(get_ad)
 routes.post('/advertisements')(create_ad)
 routes.patch('/advertisements/{id:\d+}/')(patch_ad)
